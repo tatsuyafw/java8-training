a21eeec46657a96ad85a0ce8f177369d8ce4c731
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2014-10-22 19:47:42 +0900

    Add chapter2/exercise07

---
+++ src/chapter2/exercise07/StreamUtils.java
@@ -0,0 +1,9 @@
+package chapter2.exercise07;
+
+import java.util.stream.Stream;
+
+public class StreamUtils<T> {
+    public static <T> boolean isFinite(Stream<T> stream) {
+        throw new UnsupportedOperationException("Cannot implement this method");
+    }
+}
