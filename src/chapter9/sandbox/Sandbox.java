a8bbc455c073c8595f528bd4870ea5d2efbdc644
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-01-03 17:57:50 +0900

    Add sandbox codes

--- src/chapter9/sandbox/Sandbox.java
+++ src/chapter9/sandbox/Sandbox.java
@@ -7,8 +7,20 @@ public class Sandbox {
     public static void main(String[] args) {
         Path absolute = Paths.get("/", "home");
         Path relative = Paths.get("myprog", "conf", "user.properties");
+        Path configPath = absolute.resolve(relative);
+        Path workPath = Paths.get("/", "home", "cay", "myprog", "work");
+        Path tempPath = workPath.resolveSibling("temp");
+
+        Path relativizedPath = workPath.relativize(Paths.get("/home/fred/myprog"));
+        Path normalizedPath = Paths.get("/home/cay/../fred/./myprog").normalize();
 
         System.out.println(absolute);
         System.out.println(relative);
+        System.out.println(configPath);
+        System.out.println(workPath);
+        System.out.println(tempPath);
+        System.out.println(relativizedPath);
+        System.out.println(normalizedPath);
+        System.out.println(Paths.get("config").toAbsolutePath());
     }
 }
