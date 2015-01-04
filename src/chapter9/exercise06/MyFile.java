6b7127ce6e13fae7164c3257d419e434fa5d3e31
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-01-04 18:36:31 +0900

    Add ex06

---
+++ src/chapter9/exercise06/MyFile.java
@@ -0,0 +1,34 @@
+package chapter9.exercise06;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class MyFile {
+    public static void main(String[] args) {
+        Path in = Paths.get(MyFile.class.getResource("alice.txt").getPath());
+
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(in);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        Collections.reverse(lines);
+
+        Path thisClassPath = Paths.get(MyFile.class.getResource("MyFile.class").getPath());
+        Path out = thisClassPath.resolveSibling("ecila.txt");
+        try {
+            Files.write(out, lines);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Writed to " + out);
+    }
+}
