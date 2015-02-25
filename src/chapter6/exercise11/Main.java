abb34252bc47b0d24d475297a1bfca12d6723811
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-02-26 08:05:34 +0900

    [WIP] Add sample code

--- src/chapter6/exercise11/Main.java
+++ src/chapter6/exercise11/Main.java
@@ -1,11 +1,77 @@
 package chapter6.exercise11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.PasswordAuthentication;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+/*
+ * This code is a sample.
+ * DO NOT USE for production.
+ */
 public class Main {
+    private final static String SAMPLE_PASSWORD = "secret";
+
     public static void main(String[] args) {
+        readUserPass();
+        checkPassword();
+        try {
+            ForkJoinPool.commonPool().awaitTermination(10,  TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // WIP
+    private static Supplier<PasswordAuthentication> readUserPass() {
+        return () -> {
+            String username = "", password = "";
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                String line = null;
+                // Read a username.
+                while ( true ) {
+                    System.out.print("Username: ");
+                    line = reader.readLine();
+                    if ( ! line.isEmpty() ) {
+                        username = line;
+                        break;
+                    }
+                }
+
+                // Read a password.
+                // TODO: Do not echo back.
+                while ( true ) {
+                    System.out.print("Password: ");
+                    line = reader.readLine();
+                    if ( ! line.isEmpty() ) {
+                        password = line;
+                        break;
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            return new PasswordAuthentication(username, password.toCharArray());
+        };
+    }
+
+    // WIP
+    private static Predicate<PasswordAuthentication> checkPassword() {
+        return (passwordAuthentication) -> {
+            String password = String.valueOf(passwordAuthentication.getPassword());
+            if ( password != SAMPLE_PASSWORD ) {
+              return true;
+          } else {
+              return false;
+          }
+        };
     }
 
     public static <T> CompletableFuture<T> repeat(Supplier<T> action, Predicate<T> until) {
