82b837e55835ba22905c121dfa65bd1578fdd4cf
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-02-27 15:26:12 +0900

    Add sandbox02 to chapter6

--- src/chapter6/sandbox/Sandbox.java
+++ src/chapter6/sandbox/Sandbox.java
@@ -2,9 +2,34 @@ package chapter6.sandbox;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class Sandbox {
     public static void main(String[] args) {
+        sandbox2();
+    }
+
+    public static void sandbox2() {
+        CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Finished");
+            return "CF";
+        });
+
+        cf.thenAccept(System.out::println);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sandbox1() {
         String[] words = { "abc", "abc", "bdc", "a", "x", "y", "z", "z", "y", "abc" };
         Map<String, Integer> wordCounter = new HashMap<>();
         for (String word : words) {
@@ -14,5 +39,6 @@ public class Sandbox {
         for (Map.Entry<String, Integer> e : wordCounter.entrySet()) {
             System.out.println(e.getKey() + " : " + e.getValue());
         }
+
     }
 }
