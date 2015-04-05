726935e6b9c8e32a11e2b66adc5ddc0cd3866974
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-04-06 08:34:13 +0900

    Add integers and doubles

--- src/chapter8/exercise09/ScannerConverter.java
+++ src/chapter8/exercise09/ScannerConverter.java
@@ -15,7 +15,6 @@ public class ScannerConverter {
 
     public static Stream<String> lines(Scanner scanner) {
         Iterator<String> iter = new Iterator<String>() {
-
             @Override
             public boolean hasNext() {
                 return scanner.hasNext();
@@ -26,7 +25,38 @@ public class ScannerConverter {
                 return scanner.next();
             }
         };
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                iter, Spliterator.ORDERED | Spliterator.NONNULL), false);
+    }
 
+    public static Stream<Integer> integers(Scanner scanner) {
+        Iterator<Integer> iter = new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return scanner.hasNextInt();
+            }
+
+            @Override
+            public Integer next() {
+                return scanner.nextInt();
+            }
+        };
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                iter, Spliterator.ORDERED | Spliterator.NONNULL), false);
+    }
+
+    public static Stream<Double> doubles(Scanner scanner) {
+        Iterator<Double> iter = new Iterator<Double>() {
+            @Override
+            public boolean hasNext() {
+                return scanner.hasNextDouble();
+            }
+
+            @Override
+            public Double next() {
+                return scanner.nextDouble();
+            }
+        };
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
                 iter, Spliterator.ORDERED | Spliterator.NONNULL), false);
     }
