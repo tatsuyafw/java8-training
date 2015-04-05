b7425797979637be07947b61fa16c2a7c5620c2d
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-04-06 08:25:24 +0900

    A temporary commit

---
+++ src/chapter8/exercise09/ScannerConverter.java
@@ -0,0 +1,33 @@
+package chapter8.exercise09;
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class ScannerConverter {
+    public static Stream<String> words(Scanner scanner) {
+
+        return null; // Temporary
+    }
+
+    public static Stream<String> plines(Scanner scanner) {
+        Iterator<String> iter = new Iterator<String>() {
+
+            @Override
+            public boolean hasNext() {
+                return scanner.hasNext();
+            }
+
+            @Override
+            public String next() {
+                return scanner.next();
+            }
+        };
+
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                iter, Spliterator.ORDERED | Spliterator.NONNULL), false);
+    }
+}
